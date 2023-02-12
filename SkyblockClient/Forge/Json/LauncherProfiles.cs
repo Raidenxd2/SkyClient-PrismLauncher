05348943cf0957e97d463d30377f3ac2629cbf54
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2023-02-12 14:18:37 -0500

    Add project files.

---
+++ SkyblockClient/Forge/Json/LauncherProfiles.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace SkyblockClient.Json
+{
+	public class LauncherProfiles
+	{
+		public Dictionary<string, object> profiles { get; set; }
+		public object settings { get; set; }
+	}
+}
